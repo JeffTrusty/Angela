fee6a533d6f331ab09221ec18ace0eab4136bfbe
Jeff Trusty <jeff.trusty@optum.com>
2020-09-24 17:08:30 -0600

    9-24

---
+++ 03-JavaScript/app/app.js
@@ -0,0 +1,51 @@
+/*
+var output = [];
+var count = 1;
+
+while(count <= 100) {
+  fizzBuzz();
+}
+ console.log(output);
+
+function fizzBuzz() {
+  if (count % 3 === 0 && count % 5 === 0)
+  {
+    output.push("FizzBuzz");
+  } else if (count % 3 === 0)
+  {
+    output.push("Fizz");
+  } else if (count % 5 === 0)
+  {
+    output.push("Buzz");
+  } else
+  {
+    output.push(count);
+  }
+  count++;
+}
+*/
+
+/*
+names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+function whosPaying(names) {
+  return names[Math.floor(Math.random() * names.length)] + " is going to buy lunch today!";
+}
+*/
+/*
+function fibonacciGenerator(n) {
+  if (n === 1)
+  {
+    return [0];
+  } else if (n === 2)
+  {
+    return [0, 1];
+  }
+  var returnArray = [0, 1];
+  while (returnArray.length < n)
+  {
+    returnArray.push(returnArray[returnArray.length-2] + returnArray[returnArray.length-1]);
+  }
+  return returnArray;
+}
+*/
+
