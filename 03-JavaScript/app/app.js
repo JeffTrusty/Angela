1bc36aba254eaac73b8330afa4fe9acf2fe27d7a
Jeff Trusty <jeff.trusty@optum.com>
2020-10-30 10:52:05 -0600

    added sound files

--- 03-JavaScript/app/app.js
+++ 03-JavaScript/app/app.js
@@ -48,15 +48,15 @@ function fibonacciGenerator(n) {
   return returnArray;
 }
 */
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document
-      .querySelectorAll('.drum')
-      [i].addEventListener('click', function () {
-        // alert('I got clicked');
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
-      });
-  }
+var numberOfDrumButtons = document.querySelectorAll('.drum').length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+    // alert('I got clicked');
+    var audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
+  });
+}
 
 /*
 function add(num1, num2) {
